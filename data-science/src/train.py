301c9dec31cd0ce231c11c725efb2f8caf63f92e
herrbod <Harald_Bodenhofer@hotmail.com>
2025-03-25 18:05:56 +0000

    Update train.py
    update the training module

--- data-science/src/train.py
+++ data-science/src/train.py
@@ -20,8 +20,13 @@ def parse_args():
     # -------- WRITE YOUR CODE HERE --------
     
     # Step 1: Define arguments for train data, test data, model output, and RandomForest hyperparameters. Specify their types and defaults.  
-
-
+    parser.add_argument("--train_data", type=str, help="Path to train data")
+    parser.add_argument("--test_data", type=str, help="Path to test data")
+    parser.add_argument('--n_estimators', type=int, default=100,
+                        help='The number of trees in the forest')
+    parser.add_argument('--max_depth', type=int, default=None,
+                        help='The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.')                    
+    parser.add_argument("--model_output", type=str, help="Path of output model")
     args = parser.parse_args()
 
     return args
@@ -31,13 +36,41 @@ def main(args):
 
     # -------- WRITE YOUR CODE HERE --------
 
-    # Step 2: Read the train and test datasets from the provided paths using pandas. Replace '_______' with appropriate file paths and methods.  
-    # Step 3: Split the data into features (X) and target (y) for both train and test datasets. Specify the target column name.  
-    # Step 4: Initialize the RandomForest Regressor with specified hyperparameters, and train the model using the training data.  
-    # Step 5: Log model hyperparameters like 'n_estimators' and 'max_depth' for tracking purposes in MLflow.  
-    # Step 6: Predict target values on the test dataset using the trained model, and calculate the mean squared error.  
-    # Step 7: Log the MSE metric in MLflow for model evaluation, and save the trained model to the specified output path.  
+    # Step 2: Read the train and test datasets from the provided paths using pandas. Replace '_______' with appropriate file paths and methods.
+    train_df = pd.read_csv(Path(args.train_data)/"train.csv")
+    test_df = pd.read_csv(Path(args.test_data)/"test.csv")
+    
+    # Step 3: Split the data into features (X) and target (y) for both train and test datasets. Specify the target column name.
+    # Dropping the label column and assigning it to y_train
+    y_train = train_df["price"].values  # 'price' is the target variable in this case study
 
+    # Dropping the 'price' column from train_df to get the features and converting to array for model training
+    X_train = train_df.drop("price", axis=1).values
+
+    # Dropping the label column and assigning it to y_test
+    y_test = test_df["price"].values  # 'price' is the target variable for testing
+
+    # Dropping the 'price' column from test_df to get the features and converting to array for model testing
+    X_test = test_df.drop("price", axis=1).values
+
+    # Step 4: Initialize the RandomForest Regressor with specified hyperparameters, and train the model using the training data.
+    tree_model = RandomForestRegressor(n_estimators=args.n_estimators, max_depth=args.max_depth, random_state=42)
+    tree_model = tree_model.fit(X_train, y_train)
+
+    # Step 5: Log model hyperparameters like 'n_estimators' and 'max_depth' for tracking purposes in MLflow.
+    mlflow.log_param("model", "RandomForest")  # Provide the model name
+    mlflow.log_param("n_estimators", args.n_estimators)
+    mlflow.log_param("max_depth", args.max_depth)
+
+    # Step 6: Predict target values on the test dataset using the trained model, and calculate the mean squared error.
+    yhat_test = tree_model.predict(X_test)
+    
+    # Step 7: Log the MSE metric in MLflow for model evaluation, and save the trained model to the specified output path.  
+    mse = mean_squared_error(y_test, yhat_test)
+    print('Mean Squared Error of RandomForest Regressor on test set: {:.2f}'.format(mse))
+    mlflow.log_metric("MSE", float(mse))  # Log the MSE
+
+    mlflow.sklearn.save_model(sk_model=tree_model, path=args.model_output)  # Save the model
 
 if __name__ == "__main__":
     
